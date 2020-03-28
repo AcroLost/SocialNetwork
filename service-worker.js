96bc1cdb8913e1f84639a4f578311ce199291be7
AcroLost <49655180+AcroLost@users.noreply.github.com>
2020-03-28 10:51:54 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SocialNetwork/precache-manifest.dbfe3cfb9ef2056e7e7c7db0cbf4ff35.js"
+  "/SocialNetwork/precache-manifest.ed8688b6098406be0e2789df53df1211.js"
 );
 
 self.addEventListener('message', (event) => {
